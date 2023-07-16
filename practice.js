4f55a0a04109fa0b50568a228bdc923526b47ca0
Rasheed Olayanju <dimexylora@gmail.com>
2023-07-16 15:56:17 +0100

    Some files deleted

--- practice.js
+++ practice.js
@@ -8,7 +8,6 @@ const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
 const flash = require('connect-flash');
 const multer = require('multer');
-const mongodbConfig = require('./util/mongodb.config');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
